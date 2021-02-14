591aebd95c62d68b523c4fc339322f829d45fb03
minhajul-karim <minhajul.kaarim@gmail.com>
2021-02-14 09:12:38 +0600

    Make snake of len 2 by default

--- snake/index.js
+++ snake/index.js
@@ -1,10 +1,12 @@
 const gameContainer = document.querySelector('.game-container')
 const scoreText = document.querySelector('.score-text')
 const rows = 20
-const snake = [0]
+const snake = [2, 1, 0]
 const cells = []
 let steps = 1
 let intervalId = null
+let foodIndex = null
+let score = 0
 
 // Create cells
 const cell = document.createElement('div')
@@ -17,12 +19,13 @@ for (let i = 1; i <= rows * rows; i++) {
 
 /*
   TODOS
-  1. Create snake
-  2. Auto move snake
-  3. Move snake with key
-  4. Handle gameover cases
-  5. Create food
-  6. Eat food
+  1. Create snake (done)
+  2. Auto move snake (done)
+  3. Move snake with key (done)
+  4. Create food
+  5. Increase snake
+  6. Handle gameover cases
+  7. Eat food
 */
 
 // Draw snake
@@ -30,11 +33,37 @@ snake.forEach(index => cells[index].classList.add('snake'))
 
 // Move snake
 const move = () => {
-  const head = snake[0]
-  cells[head].classList.remove('snake')
-  snake[0] = head + steps
-  cells[snake[0]].classList.add('snake')
+  const tail = snake.pop()
+  cells[tail].classList.remove('snake')
+  const newHead = snake[0] + steps
+  snake.unshift(newHead)
+  cells[newHead].classList.add('snake')
 }
 
-// intervalId = setInterval(move, 1000)
+// Control snake with keyboard
+document.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'ArrowRight':
+      steps = 1
+      break
+    case 'ArrowLeft':
+      steps = -1
+      break
+    case 'ArrowUp':
+      steps = -rows
+      break
+    case 'ArrowDown':
+      steps = rows
+      break
+  }
+})
 
+intervalId = setInterval(move, 500)
+
+// Generate food
+const generateFood = () => {
+  foodIndex = Math.floor((Math.random() * 400))
+  cells[foodIndex].classList.add('food')
+}
+
+generateFood()
